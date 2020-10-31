d969a3dd45ea6126b80e631e51c5a548d48a81f1
vagrant <vagrant@localhost.localdomain>
2020-10-31 06:39:11 +0000

    add:post_image

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,6 +7,7 @@ class PostImagesController < ApplicationController
   	@post_image = PostImage.new(post_image_params)
   	@post_image.user_id = current_user.id
   	@post_image.save
+  	redirect_to @post_image, notice: "title 「#{@post_image.title}」を保存しました。"
   end
 
   def index
@@ -23,13 +24,20 @@ class PostImagesController < ApplicationController
 
   def update
   	@post_image = PostImage.find(params[:id])
-  	@post.update(post_image_params)
+  	@post_image.update(post_image_params)
+  	redirect_to post_images_path, notice: "title 「#{@post_image.title}」を更新しました。"
+  end
+
+  def destroy
+  	@post_image = PostImage.find(params[:id])
+  	@post_image.destroy
+  	redirect_to post_images_url, notice: "title 「#{@post_image.title}」を削除しました。"
   end
 
   private
 
   def post_image_params
-  	params.require(:post_image).permit(:title, :body, :post_image)
+  	params.require(:post_image).permit(:title, :body, :image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3aeeb8689d53714c6b222c881aea0e75aff16351879adfbbfb513639791a9bdddc18976c1224a011702602189040ee3e93d9c6a08698a02d0290d8d971b28efb'
